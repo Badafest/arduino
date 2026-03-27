8e0b53ccf584a10546335d3a17a6c50b298f4f05
Sandip Dahal <er.sandipdahal@gmai.com>
2026-03-27 19:54:38 +0545

    measure led forward voltage

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
 
 uint8_t potPin = A0;
 float voltage = 0;
-LedMatrix ledMatrix(5, 6, 3, 1, 2);
+LedMatrix ledMatrix(5, 6, 3, 1, 1);
 
 void setup()
 {
   // put your setup code here, to run once:
   turnOffBuiltInLED();
   ledMatrix.init();
-  ledMatrix.displayDigit(2, 0);
 
-  Serial.begin(9600);
-  Serial.println("UNO is ready!");
+  // display 0.0
+  ledMatrix.displayDigit(0, 1);
+  ledMatrix.displayDecimal(0);
+  ledMatrix.displayDigit(0, 0);
 }
 
 void loop()
@@ -22,13 +23,14 @@ void loop()
   // put your main code here, to run repeatedly:
 
   int adcValue = analogRead(potPin);
-  voltage = adcValue * (5.0 / 1023.0);
+  float newVoltage = adcValue * (5.0 / 1023.0);
 
-  Serial.print("ADC Value:");
-  Serial.println(adcValue);
+  if (abs(newVoltage - voltage) < 0.1)
+  {
+    return;
+  }
 
-  Serial.print("Voltage:");
-  Serial.println(voltage, 3);
+  voltage = newVoltage;
 
   ledMatrix.clear();
   uint8_t voltageInt = 10 * voltage;
@@ -36,5 +38,5 @@ void loop()
   ledMatrix.displayDecimal(0);
   ledMatrix.displayDigit((voltageInt / 10) % 10, 0);
 
-  delay(1000);
+  delay(500);
 }
