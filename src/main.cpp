1196f1d5f61f33c00b1b796d648dacbd16d9839d
Sandip Dahal <er.sandipdahal@gmai.com>
2026-03-27 15:38:35 +0545

    control led through serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
 #include <Arduino.h>
 #include "helpers.h"
 
-const uint8_t buttonPin = 12;
 const uint8_t ledPin = 9;
-
 bool ledOn = false;
-
-char inChar;
-int counter = 0;
+int inInt = 0;
 
 void setup()
 {
   // put your setup code here, to run once:
   turnOffBuiltInLED();
 
-  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
 
   Serial.begin(9600);
@@ -24,33 +19,13 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, ledOn ? HIGH : LOW);
-
-  uint8_t buttonUp = digitalRead(buttonPin);
-  delay(150);
-  if (digitalRead(buttonPin) != buttonUp)
-  {
-    ledOn = !ledOn;
-  }
-}
-
-void serialEvent()
-{
   if (!Serial.available())
   {
     return;
   }
 
-  inChar = Serial.read();
+  inInt = Serial.parseInt();
   Serial.print("UNO received:");
-  Serial.println(inChar);
-
-  if (inChar == '1')
-  {
-    ledOn = true;
-  }
-  else if (inChar == '0')
-  {
-    ledOn = false;
-  }
+  Serial.println(inInt);
+  analogWrite(ledPin, constrain(inInt, 0, 255));
 }
