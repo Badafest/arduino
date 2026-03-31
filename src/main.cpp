56892dfb3fe28a35a72f5867858f71732f6f3da0
Sandip Dahal <er.sandipdahal@gmai.com>
2026-03-31 11:40:59 +0545

    buzzer through transistor

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,39 @@
 #include <Arduino.h>
 #include "helpers.h"
-#include "LedMatrix.h"
 
-uint8_t potPin = A0;
-float voltage = 0;
-LedMatrix ledMatrix(5, 6, 3, 1, 1);
+uint8_t buttonPin = 12;
+uint8_t buzzerPin = 9;
+
+float sinVal;
+float toneVal;
 
 void setup()
 {
   // put your setup code here, to run once:
   turnOffBuiltInLED();
-  ledMatrix.init();
 
-  // display 0.0
-  ledMatrix.displayDigit(0, 1);
-  ledMatrix.displayDecimal(0);
-  ledMatrix.displayDigit(0, 0);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-
-  int adcValue = analogRead(potPin);
-  float newVoltage = adcValue * (5.0 / 1023.0);
-
-  if (abs(newVoltage - voltage) < 0.1)
+  // X from 0 degree->360 degree
+  for (int x = 0; x < 360; x++)
   {
-    return;
+
+    if (digitalRead(buttonPin) == HIGH)
+    {
+      analogWrite(buzzerPin, 0);
+      break;
+    }
+
+    sinVal = sin(x * (PI / 180));  // Calculate the sine of x
+    toneVal = 2000 + sinVal * 500; // Calculate sound frequency according to the sine of x
+    tone(buzzerPin, toneVal);      // Output sound frequency to buzzerPin
+    delay(10);
   }
-
-  voltage = newVoltage;
-
-  ledMatrix.clear();
-  uint8_t voltageInt = 10 * voltage;
-  ledMatrix.displayDigit(voltageInt % 10, 1);
-  ledMatrix.displayDecimal(0);
-  ledMatrix.displayDigit((voltageInt / 10) % 10, 0);
-
-  delay(500);
 }
