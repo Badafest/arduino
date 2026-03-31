1aef32d6d80f738c25eede8a4dad1b2e68c82b58
Sandip Dahal <er.sandipdahal@gmai.com>
2026-03-31 16:14:38 +0545

    control motor with relay module

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,33 @@
 #include <Arduino.h>
 #include "helpers.h"
 
-uint8_t buttonPin = 12;
-uint8_t buzzerPin = 9;
+uint8_t buttonPin = 3;
+uint8_t relayPin = 9;
 
-float sinVal;
-float toneVal;
+int lastButtonState = LOW;
 
 void setup()
 {
   // put your setup code here, to run once:
   turnOffBuiltInLED();
 
-  pinMode(buzzerPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
 
+  digitalWrite(relayPin, HIGH);
   Serial.begin(9600);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  // X from 0 degree->360 degree
-  for (int x = 0; x < 360; x++)
+
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == lastButtonState)
   {
-
-    if (digitalRead(buttonPin) == HIGH)
-    {
-      analogWrite(buzzerPin, 0);
-      break;
-    }
-
-    sinVal = sin(x * (PI / 180));  // Calculate the sine of x
-    toneVal = 2000 + sinVal * 500; // Calculate sound frequency according to the sine of x
-    tone(buzzerPin, toneVal);      // Output sound frequency to buzzerPin
-    delay(10);
+    return;
   }
+
+  lastButtonState = buttonState;
+  digitalWrite(relayPin, 1 - lastButtonState);
 }
