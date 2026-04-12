092d2fd07e1d1ff0c94048916348f1fe11ed9d67
Sandip Dahal <er.sandipdahal@gmail.com>
2026-04-12 08:58:01 +0545

    control servo motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,26 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include "helpers.h"
-int in1Pin = 10;     // Define L293D channel 1 pin
-int in2Pin = 9;      // Define L293D channel 2 pin
-int enable1Pin = 11; // Define L293D enable 1 pin
-boolean rotationDir; // Define a variable to save the motor's rotation direction
-int rotationSpeed;   // Define a variable to save the motor rotation speed
 
-void driveMotor(boolean dir, int spd);
+uint8_t potPin = A0;
+uint8_t servoPin = 3;
+
+Servo servo;
+
+int angle = 0;
+bool angle_increasing = true;
 
 void setup()
 {
-  // Initialize the pin into an output mode:
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
+  turnOffBuiltInLED();
+
+  pinMode(potPin, INPUT);
+  servo.attach(servoPin);
 }
 
 void loop()
 {
-  int potenVal = analogRead(A0); // Convert the voltage of rotary potentiometer into digital
+  int potenVal = analogRead(A0);
 
-  // Compare the number with value 512, if more than 512, clockwise otherwise, counter clockwise
-  rotationSpeed = potenVal - 512;
-  if (potenVal > 512)
-    rotationDir = true;
-  else
-    rotationDir = false;
-
-  // Calculate the motor speed, the far number of deviation from the middle value 512, the faster the control speed will be
-  rotationSpeed = abs(potenVal - 512);
-
-  // Control the steering and speed of the motor
-  driveMotor(rotationDir, map(rotationSpeed, 0, 512, 0, 255));
-}
-
-void driveMotor(boolean dir, int spd)
-{
-  // Control motor rotation direction
-  if (rotationDir)
-  {
-    digitalWrite(in1Pin, HIGH);
-    digitalWrite(in2Pin, LOW);
-  }
-  else
-  {
-    digitalWrite(in1Pin, LOW);
-    digitalWrite(in2Pin, HIGH);
-  }
-  // Control motor rotation speed
-  analogWrite(enable1Pin, constrain(spd, 0, 255));
+  servo.write(map(potenVal, 0, 1023, 0, 180));
 }
